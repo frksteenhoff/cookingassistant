e5cb38b5a7ae7fd6a0b3821deb2002c4993daceb
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2020-05-31 20:12:02 +0200

    Added calculator

---
+++ src/components/Calculator.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<b-row class="m-2">
+			<b-col class="p-0" cols="9">
+				<b-form-input
+					v-model="equation"
+					placeholder="Lav en udregning"
+				/>
+			</b-col>
+			<b-col v-for="idx in buttonContent" :key="idx+'id'" cols="3" class="p-0 m-2">
+				<b-button>
+					{{ idx }}
+				</b-button>
+			</b-col>
+		</b-row>
+	</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+
+const AppProps = Vue.extend({
+	props: {
+	}
+})
+
+@Component({})
+export default class Calculator extends AppProps {
+	idx = 0;
+	equation = ""
+
+	get buttonContent() {
+		return Array(9).fill(1).map((x, y) => x + y).concat(["ESC", "0", ".", "*", "/", "+", "-"])
+	}
+}
+</script>
