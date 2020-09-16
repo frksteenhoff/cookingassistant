48100910592fdec46c7ecaab13e0f042e7c77465
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2020-09-16 16:10:27 +0200

    Moved dupliacted code to mixin

---
+++ src/plugin/mixin/loadImage.ts
@@ -0,0 +1,17 @@
+import { Vue, Component } from "vue-property-decorator"
+
+@Component({})
+
+export default class LoadImage extends Vue {
+	getImage() {
+		return this.recipe.image === "NaN" || !this.recipe.image ? require("@/assets/logo.png") : require("@/assets/recipes/" + this.recipe.image)
+	}
+
+	getAdditionalImage(index: number) {
+		return require("@/assets/recipes/" + this.recipe.additionalImages[index])
+	}
+
+	get recipe() {
+		return this.$store.getters.getRecipe
+	}
+}
